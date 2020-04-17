587fa7e186f5413b534931b39478e25ed63f0e3c
ishijimaitsuki <tatsuki445@gmail.com>
2020-04-17 19:57:43 +0900

    文字列クラスを定義

---
+++ lib/strings.dart
@@ -0,0 +1,11 @@
+class Strings {
+  static const AppTitle = 'inbear';
+  // ログイン画面
+  static const LoginButtonTitle = 'ログイン';
+  static const EmailLabelText = 'メールアドレス';
+  static const PasswordLabelText = 'パスワード';
+  static const CreateAccountLabelTitle = '新規登録はこちら';
+  static const PasswordForgetLabelTitle = 'パスワードをお忘れの場合はこちら';
+
+  static const emptyStringError = '必須項目です。';
+}
