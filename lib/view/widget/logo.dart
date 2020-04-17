51b64589239a743b20cc6cd4078647239e710781
ishijimaitsuki <tatsuki445@gmail.com>
2020-04-17 17:02:14 +0900

    ロゴウィジェット作成

---
+++ lib/view/widget/logo.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Logo extends StatelessWidget {
+
+  final double fontSize;
+
+  Logo({this.fontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'in',
+            style: GoogleFonts.portLligatSans(
+              textStyle: Theme.of(context).textTheme.display1,
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.pinkAccent
+            ),
+          ),
+          TextSpan(
+            text: 'bear',
+            style: GoogleFonts.portLligatSans(
+              textStyle: Theme.of(context).textTheme.display1,
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.pink[100]
+            )
+          )
+        ]
+      ),
+    );
+  }
+
+}
