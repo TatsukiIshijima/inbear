34eff0d98e31f9169f451875b474633fea083dd3
ishijimaitsuki <tatsuki445@gmail.com>
2020-04-18 15:30:56 +0900

    仮のホーム画面のレイアウト作成

---
+++ lib/view/screen/home_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:inbear_app/strings.dart';
+
+class HomePage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _selectIndex = 0;
+
+  final List<Widget> _tabs = <Widget> [
+    Text('Index 0: Home'),
+    Text('Index 1: Schedule'),
+    Text('Index 2: Settings')
+  ];
+  final List<String> _titleList = <String> [
+    Strings.HomeTitle,
+    Strings.ScheduleTitle,
+    Strings.SettingsTitle
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(_titleList.elementAt(_selectIndex),
+          style: TextStyle(
+            color: Colors.white
+          ),),
+        ),
+        body: Center(
+          child: _tabs.elementAt(_selectIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectIndex,
+          items: const <BottomNavigationBarItem> [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text(Strings.HomeTitle)
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.schedule),
+              title: Text(Strings.ScheduleTitle)
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              title: Text(Strings.SettingsTitle)
+            )
+          ],
+          onTap: (int index) {
+            setState(() {
+              _selectIndex = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+}
