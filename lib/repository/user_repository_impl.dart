d9d788b12ad73c242e979e847be68314711b4356
ishijimaitsuki <tatsuki445@gmail.com>
2020-04-19 12:53:43 +0900

    UserRepository定義

---
+++ lib/repository/user_repository_impl.dart
@@ -0,0 +1,6 @@
+class UserRepositoryImpl {
+  Future<void> signIn(String email, String password) {}
+  Future<void> signUp(String name, String email, String password) {}
+  Future<void> signOut() {}
+  Future<String> isSignIn() {}
+}
