6d0748f518a70782ac45e713fe8105639c368768
ishijimaitsuki <tatsuki445@gmail.com>
2020-04-20 16:08:25 +0900

    Firebaseの初期化を削除

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    configureFirebase()
+    //configureFirebase()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
