df0f0f60f691037588da0949bbc776c962ed108a
ishijimaitsuki <tatsuki445@gmail.com>
2020-04-26 14:42:40 +0900

    ログアウト実装

--- lib/view/screen/setting_page.dart
+++ lib/view/screen/setting_page.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:inbear_app/localize/app_localizations.dart';
+import 'package:inbear_app/repository/user_repository.dart';
+import 'package:inbear_app/view/widget/closed_question_dialog.dart';
 import 'package:inbear_app/view/widget/title_icon_list_item.dart';
+import 'package:inbear_app/viewmodel/setting_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+import '../../routes.dart';
 
 class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => SettingViewModel(
+        Provider.of<UserRepository>(context, listen: false)
+      ),
+      child: SettingPageContent(),
+    );
+  }
+}
+
+class SettingPageContent extends StatelessWidget {
+
+  void _showLogoutDialog(BuildContext context, SettingViewModel viewModel) {
+    var resource = AppLocalizations.of(context);
+    showDialog(
+        context: context,
+        builder: (context) =>
+          ClosedQuestionDialog(
+            title: resource.logoutTitle,
+            message: resource.logtoutMessage,
+            positiveButtonTitle: resource.defaultPositiveButtonTitle,
+            negativeButtonTitle: resource.defaultNegativeButtonTitle,
+            onPositiveButtonPressed: () async {
+              await viewModel.signOut();
+              Routes.goToLogin(context);
+            },
+          )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var viewModel = Provider.of<SettingViewModel>(context, listen: false);
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
@@ -17,10 +55,10 @@ class SettingPage extends StatelessWidget {
           TitleAndIconListItem(
             title: AppLocalizations.of(context).logoutTitle,
             iconData: Icons.exit_to_app,
-            onTap: () {},
+            onTap: () => _showLogoutDialog(context, viewModel),
           )
         ],
       ),
-    );
+    );;
   }
 }
