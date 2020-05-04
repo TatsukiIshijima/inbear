83f1d8197b0ac1bc7ba4ac7e0c0638c78deb2b65
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-04 17:25:24 +0900

    statusのenumからタイトルとメッセージを取得する拡張を作成

--- lib/schedule_register_status.dart
+++ lib/schedule_register_status.dart
@@ -1,3 +1,5 @@
+import 'package:inbear_app/localize/app_localizations.dart';
+
 enum ScheduleRegisterStatus {
   None,
   Loading,
@@ -10,4 +12,54 @@ enum ScheduleRegisterStatus {
   SocketError,
   UnLoginError,
   GeneralError,
+}
+
+extension ScheduleRegisterStatusExtension on ScheduleRegisterStatus {
+  static Map toTitleAndMessage(AppLocalizations resource, ScheduleRegisterStatus status) {
+    var titleAndMessage = {
+      'title': '',
+      'message': ''
+    };
+    switch (status) {
+      case ScheduleRegisterStatus.None:
+        break;
+      case ScheduleRegisterStatus.Loading:
+        break;
+      case ScheduleRegisterStatus.Success:
+        break;
+      case ScheduleRegisterStatus.UnSelectDateError:
+        titleAndMessage['title'] = resource.generalErrorTitle;
+        titleAndMessage['message'] = resource.unselectDateError;
+        break;
+      case ScheduleRegisterStatus.InvalidPostalCodeError:
+        titleAndMessage['title'] = resource.generalErrorTitle;
+        titleAndMessage['message'] = resource.invalidPostalCodeError;
+        break;
+      case ScheduleRegisterStatus.UnableSearchAddressError:
+        titleAndMessage['title'] = resource.generalErrorTitle;
+        titleAndMessage['message'] = resource.unableSearchAddressError;
+        break;
+      case ScheduleRegisterStatus.Timeout:
+        titleAndMessage['title'] = resource.connectionErrorTitle;
+        titleAndMessage['message'] = resource.timeoutError;
+        break;
+      case ScheduleRegisterStatus.HttpError:
+        titleAndMessage['title'] = resource.connectionErrorTitle;
+        titleAndMessage['message'] = resource.httpError;
+        break;
+      case ScheduleRegisterStatus.SocketError:
+        titleAndMessage['title'] = resource.connectionErrorTitle;
+        titleAndMessage['message'] = resource.socketError;
+        break;
+      case ScheduleRegisterStatus.UnLoginError:
+        titleAndMessage['title'] = resource.generalErrorTitle;
+        titleAndMessage['message'] = resource.unloginError;
+        break;
+      case ScheduleRegisterStatus.GeneralError:
+        titleAndMessage['title'] = resource.generalErrorTitle;
+        titleAndMessage['message'] = resource.generalError;
+        break;
+    }
+    return titleAndMessage;
+  }
 }
