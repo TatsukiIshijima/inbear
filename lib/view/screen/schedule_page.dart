83745f9e23d90afe574c946b0598474426c7eb17
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-06 15:01:07 +0900

    スケジュール表示画面の仮実装

--- lib/view/screen/schedule_page.dart
+++ lib/view/screen/schedule_page.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:inbear_app/localize/app_localizations.dart';
+import 'package:inbear_app/model/schedule.dart';
+import 'package:inbear_app/repository/schedule_respository.dart';
+import 'package:inbear_app/repository/user_repository.dart';
+import 'package:inbear_app/schedule_get_status.dart';
+import 'package:inbear_app/view/widget/loading.dart';
+import 'package:inbear_app/viewmodel/schedule_viewmodel.dart';
+import 'package:provider/provider.dart';
 
 class SchedulePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SchedulePageContent();
+    return ChangeNotifierProvider(
+      create: (context) => ScheduleViewModel(
+        Provider.of<UserRepository>(context, listen: false),
+        Provider.of<ScheduleRepository>(context, listen: false)
+      ),
+      child: SchedulePageContent(),
+    );
   }
 
 }
@@ -14,21 +27,50 @@ class SchedulePageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var viewModel = Provider.of<ScheduleViewModel>(context, listen: false);
     var resource = AppLocalizations.of(context);
-    return Stack(
-      children: <Widget>[
-        SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(resource.scheduleGroomNameLabelText),
-              Text(resource.scheduleBrideNameLabelText),
-              Text(resource.scheduleDateLabelText),
-              Text(resource.schedulePlaceLabelText)
-            ],
-          ),
-        )
-      ],
+    WidgetsBinding.instance.addPostFrameCallback((_) =>
+        viewModel.fetchSelectSchedule());
+    return Selector<ScheduleViewModel, ScheduleGetStatus>(
+      selector: (context, viewModel) => viewModel.status,
+      builder: (context, status, child) {
+        if (status == ScheduleGetStatus.None) {
+          return Container();
+        } else if (status == ScheduleGetStatus.Loading) {
+          return Center(child: Loading());
+        } else if (status == ScheduleGetStatus.Success) {
+          return Selector<ScheduleViewModel, Schedule>(
+            selector: (context, viewModel) => viewModel.schedule,
+            builder: (context, schedule, viewModel) =>
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(resource.scheduleGroomNameLabelText),
+                    Text(schedule.groom),
+                    Text(resource.scheduleBrideNameLabelText),
+                    Text(schedule.bride),
+                    Text(resource.scheduleDateLabelText),
+                    Text(schedule.dateTime.toString()),
+                    Text(resource.schedulePlaceLabelText),
+                    Text(schedule.address),
+                  ],
+                ),
+              ),
+          );
+        } else {
+          var titleAndMessage = ScheduleGetStatusExtension
+              .toTitleAndMessage(resource, status);
+          return Center(
+            child: Text(
+              titleAndMessage['messsage'],
+              style: TextStyle(
+                fontSize: 20
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 
