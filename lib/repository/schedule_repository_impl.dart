6aedb4ab481dd7e644b900df43172c7bd722e464
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-06 14:12:43 +0900

    スケジュール取得メソッド追加

--- lib/repository/schedule_repository_impl.dart
+++ lib/repository/schedule_repository_impl.dart
@@ -2,4 +2,5 @@ import 'package:inbear_app/model/schedule.dart';
 
 class ScheduleRepositoryImpl {
   Future<String> registerSchedule(Schedule schedule) {}
+  Future<Schedule> fetchSchedule(String selectScheduleId) {}
 }
