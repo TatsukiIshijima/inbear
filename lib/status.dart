2c60fba1551a368a166ad6c4a2914003df083aec
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-08 15:06:14 +0900

    共通のステータスの定数群作成

---
+++ lib/status.dart
@@ -0,0 +1,7 @@
+class Status {
+  static const String none = 'NONE';
+  static const String loading = 'LOADING';
+  static const String success = 'SUCCESS';
+  static const String unLoginError = 'UNLOGIN_ERROR';
+  static const String generalError = 'GENERAL_ERROR';
+}
