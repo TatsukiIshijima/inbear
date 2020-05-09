7c2bf298a32715171dc9ffca4c89877830157bd9
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-09 15:32:27 +0900

    ステータス追加

--- lib/status.dart
+++ lib/status.dart
@@ -4,5 +4,8 @@ class Status {
   static const String success = 'SUCCESS';
   static const String unLoginError = 'UNLOGIN_ERROR';
   static const String notExistDocumentError = 'NOT_EXIST_DOCUMENT_ERROR';
+  static const String timeoutError = 'TIME_OUT_ERROR';
+  static const String httpError = 'HTTP_ERROR';
+  static const String socketError = 'SOCKET_ERROR';
   static const String generalError = 'GENERAL_ERROR';
 }
