80852cad7a5a3e864e57c8153043dfffdf35915f
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-09 14:18:20 +0900

    ステータスと例外の追加

--- lib/status.dart
+++ lib/status.dart
@@ -3,5 +3,6 @@ class Status {
   static const String loading = 'LOADING';
   static const String success = 'SUCCESS';
   static const String unLoginError = 'UNLOGIN_ERROR';
+  static const String notExistDocumentError = 'NOT_EXIST_DOCUMENT_ERROR';
   static const String generalError = 'GENERAL_ERROR';
 }
