70b756fcfcdf6cadf226e14da7fbbd72823c19c8
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-17 17:05:30 +0900

    正方形の画像になるよう修正

--- lib/view/widget/photo_item.dart
+++ lib/view/widget/photo_item.dart
@@ -7,10 +7,37 @@ class PhotoItem extends StatelessWidget {
 
   PhotoItem(this.url);
 
+  static const _imageWidth = 300.0;
+  static const _imageHeight = 300.0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Ink.image(image: ExtendedNetworkImageProvider(url)),
-    );
+        child: ExtendedImage.network(
+      url,
+      width: _imageWidth,
+      height: _imageHeight,
+      fit: BoxFit.cover,
+      cache: true,
+      loadStateChanged: (state) {
+        switch (state.extendedImageLoadState) {
+          case LoadState.loading:
+            return Center(
+              child: Icon(Icons.image),
+            );
+          case LoadState.completed:
+            return ExtendedRawImage(
+              image: state.extendedImageInfo?.image,
+              width: _imageWidth,
+              height: _imageHeight,
+              fit: BoxFit.cover,
+            );
+          case LoadState.failed:
+            return Center(child: Icon(Icons.error));
+          default:
+            return Container();
+        }
+      },
+    ));
   }
 }
