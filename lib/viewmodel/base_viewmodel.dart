b22ed5affcd5ba1ab8219929dcfc84b65ec16319
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-21 21:26:43 +0900

    キャンセル時の処理を追加できるよう修正

--- lib/viewmodel/base_viewmodel.dart
+++ lib/viewmodel/base_viewmodel.dart
@@ -1,14 +1,20 @@
+import 'dart:async';
+
 import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 
 class BaseViewModel extends ChangeNotifier {
   CancelableOperation _cancelableOperation;
 
-  Future<dynamic> fromCancelable(Future<dynamic> future) async {
+  Future<dynamic> fromCancelable(Future<dynamic> future,
+      {FutureOr Function() onCancel}) async {
     await _cancelableOperation?.cancel();
     _cancelableOperation =
         CancelableOperation<dynamic>.fromFuture(future, onCancel: () {
       debugPrint('Operation Canceled.');
+      if (onCancel != null) {
+        onCancel();
+      }
     });
     return _cancelableOperation.value;
   }
