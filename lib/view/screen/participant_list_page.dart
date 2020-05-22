fe8b0019be6c04949c3d04e30bdd2a0a8ca1987d
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-22 23:03:13 +0900

    参加者の表示

--- lib/view/screen/participant_list_page.dart
+++ lib/view/screen/participant_list_page.dart
@@ -1,34 +1,72 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:inbear_app/entity/user_entity.dart';
+import 'package:inbear_app/repository/schedule_respository.dart';
+import 'package:inbear_app/repository/user_repository.dart';
+import 'package:inbear_app/view/widget/loading.dart';
 import 'package:inbear_app/view/widget/participant_item.dart';
+import 'package:inbear_app/viewmodel/participant_list_viewmodel.dart';
+import 'package:provider/provider.dart';
 
 class ParticipantListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ParticipantListContent(),
-      floatingActionButton: AddParticipantButton(),
+    return ChangeNotifierProvider(
+      create: (context) => ParticipantListViewModel(
+          Provider.of<UserRepository>(context, listen: false),
+          Provider.of<ScheduleRepository>(context, listen: false)),
+      child: Scaffold(
+        body: ParticipantList(),
+        floatingActionButton: AddParticipantButton(),
+      ),
     );
   }
 }
 
-class ParticipantListContent extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ParticipantList();
-  }
-}
-
 class ParticipantList extends StatelessWidget {
+  Widget _errorText(String errorMessage) {
+    return Text(
+      errorMessage,
+      textAlign: TextAlign.center,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => ParticipantItem(
-              userName: 'サンプル',
-              email: 'sample@example.com',
-              onTap: () {},
-            ));
+    final viewModel =
+        Provider.of<ParticipantListViewModel>(context, listen: false);
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      await viewModel.fetchParticipantsStart();
+    });
+    return StreamBuilder<List<UserEntity>>(
+      initialData: null,
+      stream: viewModel.participantsStream,
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Center(child: Loading());
+          default:
+            if (snapshot.hasError) {
+              return Center(
+                child: _errorText('${snapshot.error}'),
+              );
+            } else if (!snapshot.hasData) {
+              return Center(
+                child: _errorText('データなし'),
+              );
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  controller: viewModel.scrollController,
+                  itemBuilder: (context, index) => ParticipantItem(
+                        userName: snapshot.data[index].name,
+                        email: snapshot.data[index].email,
+                        onTap: () {},
+                      ));
+            }
+        }
+      },
+    );
   }
 }
 
