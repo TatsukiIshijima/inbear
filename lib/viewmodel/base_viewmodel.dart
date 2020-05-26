4d005a0ddde5f2228733bec46e23dab71dd002c1
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-26 21:43:12 +0900

    BaseViewModelに共通の処理追加

--- lib/viewmodel/base_viewmodel.dart
+++ lib/viewmodel/base_viewmodel.dart
@@ -1,10 +1,16 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:async/async.dart';
 import 'package:flutter/material.dart';
+import 'package:inbear_app/custom_exceptions.dart';
+
+import '../status.dart';
 
 class BaseViewModel extends ChangeNotifier {
   CancelableOperation _cancelableOperation;
+
+  String status = Status.none;
 
   Future<dynamic> fromCancelable(Future<dynamic> future,
       {FutureOr Function() onCancel}) async {
@@ -17,6 +23,30 @@ class BaseViewModel extends ChangeNotifier {
       }
     });
     return _cancelableOperation.value;
+  }
+
+  Future<void> futureWithCancel(Future<void> Function() future) async {
+    try {
+      status = Status.loading;
+      notifyListeners();
+      await fromCancelable(future());
+      status = Status.success;
+    } on UnLoginException {
+      status = Status.unLoginError;
+    } on UserDocumentNotExistException {
+      status = Status.userDocumentNotExistError;
+    } on ScheduleDocumentNotExistException {
+      status = Status.scheduleDocumentNotExistError;
+    } on TimeoutException {
+      status = Status.timeoutError;
+    } on HttpException {
+      status = Status.httpError;
+    } on SocketException {
+      status = Status.socketError;
+    } on NetworkRequestException {
+      status = Status.networkError;
+    }
+    notifyListeners();
   }
 
   @override
