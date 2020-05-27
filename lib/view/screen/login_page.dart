0d5aa00ab4c524d568cb1325f025ff06f246964b
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-27 22:18:27 +0900

    リファクタ

--- lib/view/screen/login_page.dart
+++ lib/view/screen/login_page.dart
@@ -28,6 +28,23 @@ class LoginPage extends StatelessWidget {
 }
 
 class LoginPageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(
+        children: <Widget>[
+          Center(
+              child: SingleChildScrollView(
+                child: LoginForm(),
+          )),
+          AuthAlertDialog(),
+        ],
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
   final _emailFocus = FocusNode();
   final _passwordFocus = FocusNode();
@@ -36,102 +53,87 @@ class LoginPageBody extends StatelessWidget {
   Widget build(BuildContext context) {
     final resource = AppLocalizations.of(context);
     final viewModel = Provider.of<LoginViewModel>(context, listen: false);
-    return SingleChildScrollView(
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-        child: Stack(
-          children: <Widget>[
-            Form(
-              key: _formKey,
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Logo(
-                      fontSize: 80,
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Selector<LoginViewModel, TextEditingController>(
-                      selector: (context, viewModel) =>
-                          viewModel.emailTextEditingController,
-                      builder: (context, textEditingController, child) =>
-                          InputField(
-                        labelText: resource.emailLabelText,
-                        textInputType: TextInputType.emailAddress,
-                        textEditingController: textEditingController,
-                        validator: (text) =>
-                            text.isEmpty ? resource.emptyError : null,
-                        focusNode: _emailFocus,
-                        onFieldSubmitted: (text) =>
-                            FocusScope.of(context).requestFocus(_passwordFocus),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Selector<LoginViewModel, TextEditingController>(
-                      selector: (context, viewModel) =>
-                          viewModel.passwordTextEditingController,
-                      builder: (context, textEditingController, child) =>
-                          InputField(
-                        labelText: resource.passwordLabelText,
-                        obscureText: true,
-                        textInputType: TextInputType.visiblePassword,
-                        textEditingController: textEditingController,
-                        validator: (text) =>
-                            text.isEmpty ? resource.emptyError : null,
-                        focusNode: _passwordFocus,
-                        onFieldSubmitted: (text) => _passwordFocus.unfocus(),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    RoundButton(
-                      minWidth: MediaQuery.of(context).size.width,
-                      text: resource.loginButtonTitle,
-                      backgroundColor: Colors.pink[200],
-                      onPressed: () async {
-                        if (_formKey.currentState.validate()) {
-                          await viewModel.executeSignIn();
-                        }
-                      },
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    LabelButton(
-                      text: resource.passwordForgetLabelText,
-                      onTap: () {
-                        Routes.goToResetPasswordFromLogin(context);
-                        viewModel.resetAuthStatus();
-                      },
-                    ),
-                  ],
+    return Container(
+        margin: EdgeInsets.symmetric(horizontal: 24),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Logo(
+                fontSize: 80,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Selector<LoginViewModel, TextEditingController>(
+                selector: (context, viewModel) =>
+                    viewModel.emailTextEditingController,
+                builder: (context, textEditingController, child) => InputField(
+                  labelText: resource.emailLabelText,
+                  textInputType: TextInputType.emailAddress,
+                  textEditingController: textEditingController,
+                  validator: (text) =>
+                      text.isEmpty ? resource.emptyError : null,
+                  focusNode: _emailFocus,
+                  onFieldSubmitted: (text) =>
+                      FocusScope.of(context).requestFocus(_passwordFocus),
                 ),
               ),
-            ),
-            Container(
-              alignment: Alignment.bottomCenter,
-              margin: EdgeInsets.only(bottom: 24),
-              child: SafeArea(
-                child: LabelButton(
-                  text: resource.createAccountLabelText,
-                  onTap: () {
-                    Routes.goToRegisterFromLogin(context);
-                    viewModel.resetAuthStatus();
-                  },
+              SizedBox(
+                height: 30,
+              ),
+              Selector<LoginViewModel, TextEditingController>(
+                selector: (context, viewModel) =>
+                    viewModel.passwordTextEditingController,
+                builder: (context, textEditingController, child) => InputField(
+                  labelText: resource.passwordLabelText,
+                  obscureText: true,
+                  textInputType: TextInputType.visiblePassword,
+                  textEditingController: textEditingController,
+                  validator: (text) =>
+                      text.isEmpty ? resource.emptyError : null,
+                  focusNode: _passwordFocus,
+                  onFieldSubmitted: (text) => _passwordFocus.unfocus(),
                 ),
               ),
-            ),
-            AuthAlertDialog(),
-          ],
-        ),
-      ),
-    );
+              SizedBox(
+                height: 30,
+              ),
+              RoundButton(
+                minWidth: MediaQuery.of(context).size.width,
+                text: resource.loginButtonTitle,
+                backgroundColor: Colors.pink[200],
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    await viewModel.executeSignIn();
+                  }
+                },
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              LabelButton(
+                text: resource.passwordForgetLabelText,
+                onTap: () {
+                  Routes.goToResetPasswordFromLogin(context);
+                  viewModel.resetAuthStatus();
+                },
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              LabelButton(
+                text: resource.createAccountLabelText,
+                onTap: () {
+                  Routes.goToRegisterFromLogin(context);
+                  viewModel.resetAuthStatus();
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
 
