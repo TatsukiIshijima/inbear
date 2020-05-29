0ec7f39ea6aaf57cf7685fd6c240fa984ae46772
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-29 20:35:12 +0900

    抽象クラスに修正

--- lib/repository/user_repository_impl.dart
+++ lib/repository/user_repository_impl.dart
@@ -3,22 +3,22 @@ import 'package:inbear_app/entity/schedule_entity.dart';
 import 'package:inbear_app/entity/user_entity.dart';
 import 'package:inbear_app/model/schedule_select_item_model.dart';
 
-class UserRepositoryImpl {
-  Future<void> signIn(String email, String password) {}
+abstract class UserRepositoryImpl {
+  Future<void> signIn(String email, String password);
   Future<FirebaseUser> createUserWithEmailAndPassword(
-      String email, String password) {}
-  Future<void> insertNewUser(FirebaseUser user, String name) {}
-  Future<void> signOut() {}
-  Future<bool> isSignIn() {}
-  Future<void> sendPasswordResetEmail(String email) {}
-  Future<String> getUid() {}
-  Future<UserEntity> fetchUser() {}
+      String email, String password);
+  Future<void> insertNewUser(FirebaseUser user, String name);
+  Future<void> signOut();
+  Future<bool> isSignIn();
+  Future<void> sendPasswordResetEmail(String email);
+  Future<String> getUid();
+  Future<UserEntity> fetchUser();
   Future<void> registerSchedule(
-      String scheduleId, ScheduleEntity scheduleEntity) {}
-  Future<void> selectSchedule(String scheduleId) {}
-  Future<List<ScheduleSelectItemModel>> fetchEntrySchedule() {}
-  Future<List<UserEntity>> searchUser(String email) {}
+      String scheduleId, ScheduleEntity scheduleEntity);
+  Future<void> selectSchedule(String scheduleId);
+  Future<List<ScheduleSelectItemModel>> fetchEntrySchedule();
+  Future<List<UserEntity>> searchUser(String email);
   Future<void> addScheduleInTargetUser(
-      String targetUid, String targetScheduleId) {}
-  Future<void> deleteSchedule(String targetUid, String targetScheduleId) {}
+      String targetUid, String targetScheduleId);
+  Future<void> deleteSchedule(String targetUid, String targetScheduleId);
 }
