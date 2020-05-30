ccba93590a1b30f3cfeba5a31db5fe9e16e9895c
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-30 22:03:25 +0900

    レイアウト修正

--- lib/view/screen/prepare_page.dart
+++ lib/view/screen/prepare_page.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:inbear_app/routes.dart';
+import 'package:inbear_app/view/widget/loading.dart';
+import 'package:inbear_app/view/widget/logo.dart';
 
 class PreparePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-      await Future<void>.delayed(Duration(milliseconds: 500));
+      await Future<void>.delayed(Duration(milliseconds: 750));
       Routes.goToHome(context);
     });
     return Scaffold(
       body: Center(
-        child: Text('切り替え中...'),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Logo(
+              fontSize: 80,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Loading(),
+            const SizedBox(height: 10),
+            const Text(
+              '設定中...',
+              style: TextStyle(fontSize: 22),
+            )
+          ],
+        ),
       ),
     );
   }
