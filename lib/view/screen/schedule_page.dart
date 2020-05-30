7479f251549e51477f081903e35a8fe0264907ec
ishijimaitsuki <tatsuki445@gmail.com>
2020-05-30 11:32:32 +0900

    スケジュール画面リファクタ

--- lib/view/screen/schedule_page.dart
+++ lib/view/screen/schedule_page.dart
@@ -6,173 +6,139 @@ import 'package:inbear_app/localize/app_localizations.dart';
 import 'package:inbear_app/repository/schedule_respository.dart';
 import 'package:inbear_app/repository/user_repository.dart';
 import 'package:inbear_app/status.dart';
+import 'package:inbear_app/view/screen/base_page.dart';
+import 'package:inbear_app/view/widget/centering_error_message.dart';
 import 'package:inbear_app/view/widget/label.dart';
-import 'package:inbear_app/view/widget/loading.dart';
+import 'package:inbear_app/view/widget/reload_button.dart';
 import 'package:inbear_app/viewmodel/schedule_viewmodel.dart';
 import 'package:provider/provider.dart';
 
 class SchedulePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final resource = AppLocalizations.of(context);
-    return ChangeNotifierProvider(
-      create: (context) => ScheduleViewModel(
+    return BasePage<ScheduleViewModel>(
+      viewModel: ScheduleViewModel(
           Provider.of<UserRepository>(context, listen: false),
           Provider.of<ScheduleRepository>(context, listen: false)),
-      child: SchedulePageContent(resource),
+      child: SchedulePageBody(),
     );
   }
 }
 
-class SchedulePageContent extends StatelessWidget {
-  final AppLocalizations resource;
-
-  SchedulePageContent(this.resource);
-
-  Widget _scheduleContent(
-      AppLocalizations resource, ScheduleViewModel viewModel) {
-    return Selector<ScheduleViewModel, ScheduleEntity>(
-      selector: (context, viewModel) => viewModel.schedule,
-      builder: (context, schedule, child) => SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Label(
-                text: resource.scheduleGroomText,
-                iconData: Icons.account_circle,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 12),
-                child: Text(
-                  schedule.groom,
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-              Label(
-                  text: resource.scheduleBrideText,
-                  iconData: Icons.account_circle),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 12),
-                child: Text(
-                  schedule.bride,
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-              Label(
-                  text: resource.scheduleDateLabelText,
-                  iconData: Icons.calendar_today),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 12),
-                child: Text(
-                  viewModel.dateToString(schedule.dateTime),
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-              Label(
-                text: resource.schedulePlaceLabelText,
-                iconData: Icons.place,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 12),
-                child: Text(
-                  schedule.address,
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width * (3 / 4),
-                child: GoogleMap(
-                  initialCameraPosition: CameraPosition(
-                    target: LatLng(schedule.geoPoint.latitude,
-                        schedule.geoPoint.longitude),
-                    zoom: 17.0,
-                  ),
-                  markers: {
-                    Marker(
-                        markerId: MarkerId('markerPoint'),
-                        position: LatLng(schedule.geoPoint.latitude,
-                            schedule.geoPoint.longitude))
-                  },
-                  mapType: MapType.normal,
-                  myLocationButtonEnabled: false,
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _errorMessage(String errorMessage) {
-    return Center(
-      child: Text(
-        errorMessage,
-        textAlign: TextAlign.center,
-      ),
-    );
-  }
-
-  Widget _reloadWidget(BuildContext context, ScheduleViewModel viewModel) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            resource.timeoutError,
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          RaisedButton(
-            onPressed: () async => await viewModel.fetchSelectSchedule(),
-            child: Text(
-              resource.reloadMessage,
-              style: TextStyle(color: Color(0xfff48fb1)),
-            ),
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                side: BorderSide(color: Color(0xfff48fb1))),
-          )
-        ],
-      ),
-    );
-  }
-
+class SchedulePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final resource = AppLocalizations.of(context);
     final viewModel = Provider.of<ScheduleViewModel>(context, listen: false);
     WidgetsBinding.instance.addPostFrameCallback(
-        (_) async => await viewModel.fetchSelectSchedule());
+        (_) async => await viewModel.executeFetchSelectSchedule());
     return Selector<ScheduleViewModel, String>(
       selector: (context, viewModel) => viewModel.status,
       builder: (context, status, child) {
         switch (status) {
-          case Status.loading:
-            return Center(child: Loading());
           case Status.unLoginError:
-            return _errorMessage(resource.unloginError);
-          case ScheduleGetStatus.notExistUserDataError:
-            return _errorMessage(resource.notExistUserDataError);
-          case ScheduleGetStatus.notExistScheduleDataError:
-            return _errorMessage(resource.notExistScheduleDataError);
+            return CenteringErrorMessage(resource,
+                message: resource.unloginError);
+          case Status.userDocumentNotExistError:
+            return CenteringErrorMessage(resource,
+                message: resource.notExistUserDataError);
+          case Status.scheduleDocumentNotExistError:
+            return CenteringErrorMessage(resource,
+                message: resource.notExistScheduleDataError);
           case ScheduleGetStatus.noSelectScheduleError:
-            return _errorMessage(resource.noSelectScheduleError);
+            return CenteringErrorMessage(resource,
+                message: resource.noSelectScheduleError);
           case Status.timeoutError:
-            return _reloadWidget(context, viewModel);
+            return ReloadButton(
+              onPressed: () async =>
+                  await viewModel.executeFetchSelectSchedule(),
+            );
           case Status.success:
-            return _scheduleContent(resource, viewModel);
+            return ScheduleDetail();
           default:
             return Container();
         }
       },
     );
+  }
+}
+
+class ScheduleDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final resource = AppLocalizations.of(context);
+    final viewModel = Provider.of<ScheduleViewModel>(context, listen: false);
+    return SingleChildScrollView(
+        child: Container(
+            margin: EdgeInsets.all(24),
+            child: Selector<ScheduleViewModel, ScheduleEntity>(
+              selector: (context, viewModel) => viewModel.schedule,
+              builder: (context, schedule, child) => Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Label(
+                    text: resource.scheduleGroomText,
+                    iconData: Icons.account_circle,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 12),
+                    child: Text(
+                      schedule.groom,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Label(
+                      text: resource.scheduleBrideText,
+                      iconData: Icons.account_circle),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 12),
+                    child: Text(
+                      schedule.bride,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Label(
+                      text: resource.scheduleDateLabelText,
+                      iconData: Icons.calendar_today),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 12),
+                    child: Text(
+                      viewModel.dateToString(schedule.dateTime),
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Label(
+                    text: resource.schedulePlaceLabelText,
+                    iconData: Icons.place,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 12),
+                    child: Text(
+                      schedule.address,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width * (3 / 4),
+                    child: GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: LatLng(schedule.geoPoint.latitude,
+                            schedule.geoPoint.longitude),
+                        zoom: 17.0,
+                      ),
+                      markers: {
+                        Marker(
+                            markerId: MarkerId('markerPoint'),
+                            position: LatLng(schedule.geoPoint.latitude,
+                                schedule.geoPoint.longitude))
+                      },
+                      mapType: MapType.normal,
+                      myLocationButtonEnabled: false,
+                    ),
+                  )
+                ],
+              ),
+            )));
   }
 }
