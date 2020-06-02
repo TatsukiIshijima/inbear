f5a5d08f39ae3f0b4e48aa8b5ded67549de6dfc2
ishijimaitsuki <tatsuki445@gmail.com>
2020-06-02 22:06:31 +0900

    画像を表示できるよう修正

--- lib/view/screen/photo_preview_page.dart
+++ lib/view/screen/photo_preview_page.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:inbear_app/entity/image_entity.dart';
 
 class PhotoPreviewPage extends StatelessWidget {
   final DocumentSnapshot documentSnapshot;
@@ -14,8 +16,21 @@ class PhotoPreviewPage extends StatelessWidget {
         title: Text('写真'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('${documentSnapshot.documentID}'),
+      body: ExtendedImageGesturePageView.builder(
+        itemBuilder: (context, index) {
+          final imageEntity = ImageEntity.fromMap(documentSnapshot.data);
+          return ExtendedImage.network(
+            imageEntity.originalUrl,
+            fit: BoxFit.contain,
+            mode: ExtendedImageMode.gesture,
+            initGestureConfigHandler: (state) => GestureConfig(
+                inPageView: true, initialScale: 1.0, cacheGesture: true),
+          );
+        },
+        itemCount: 1,
+        onPageChanged: (index) {},
+        controller: PageController(initialPage: 0),
+        scrollDirection: Axis.horizontal,
       ),
     );
   }
