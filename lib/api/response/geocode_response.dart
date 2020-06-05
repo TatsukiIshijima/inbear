0cdbeb7f84f12492ce74212443315abafec3e0e4
ishijimaitsuki <tatsuki445@gmail.com>
2020-06-05 22:26:56 +0900

    逆ジオコードの時のエラーコード追加

--- lib/api/response/geocode_response.dart
+++ lib/api/response/geocode_response.dart
@@ -1,19 +1,42 @@
 // Geocodeing API
 // https://developers.google.com/maps/documentation/geocoding/intro#GeocodingRequests
+
+import 'package:inbear_app/exception/api/api_exception.dart';
+
 class GeoCodeResponse {
   final List<GeometryEntity> geometries;
 
   GeoCodeResponse(this.geometries);
 
   static const _resultsKey = 'results';
+  static const _statusKey = 'status';
+
+  static const _ok = 'OK';
+  static const _zeroResults = 'ZERO_RESULTS';
+  static const _overDailyLimit = 'OVER_DAILY_LIMIT';
+  static const _requestDenied = 'REQUEST_DENIED';
+  static const _invalidRequest = 'INVALID_REQUEST';
 
   factory GeoCodeResponse.fromJson(Map<String, dynamic> json) {
-    final results = json[_resultsKey] as List<Object>;
-    final geometryList = results
-        .map(
-            (result) => GeometryEntity.fromJson(result as Map<String, dynamic>))
-        .toList();
-    return GeoCodeResponse(geometryList);
+    final status = json[_statusKey] as String;
+    if (status == _ok) {
+      final results = json[_resultsKey] as List<Object>;
+      final geometryList = results
+          .map((result) =>
+              GeometryEntity.fromJson(result as Map<String, dynamic>))
+          .toList();
+      return GeoCodeResponse(geometryList);
+    } else if (status == _zeroResults) {
+      throw GeocodeApiException(405);
+    } else if (status == _overDailyLimit) {
+      throw GeocodeApiException(411);
+    } else if (status == _requestDenied) {
+      throw GeocodeApiException(401);
+    } else if (status == _invalidRequest) {
+      throw GeocodeApiException(400);
+    } else {
+      throw InternalServerException();
+    }
   }
 }
 
