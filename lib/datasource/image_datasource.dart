cfc8b6b391aaf477d97fe12c3a60585e93035276
ishijimaitsuki <tatsuki445@gmail.com>
2020-06-13 20:46:39 +0900

    アップロードする画像のサイズの修正

--- lib/datasource/image_datasource.dart
+++ lib/datasource/image_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/services.dart';
 import 'package:inbear_app/datasource/image_datasource_impl.dart';
 import 'package:inbear_app/exception/storage/firebase_storage_exception.dart';
 import 'package:multi_image_picker/multi_image_picker.dart';
@@ -9,9 +10,12 @@ class ImageDataSource implements ImageDataSourceImpl {
 
   ImageDataSource(this._storage);
 
-  static const _originalImageQuality = 50;
-  static const _thumbnailImageQuality = 30;
-  static const _thumbnailImageRate = 0.2;
+  static const _originalImageQuality = 75;
+  static const _originalDefaultWidth = 720;
+  static const _originalDefaultHeight = 1280;
+  static const _thumbnailImageQuality = 75;
+  static const _thumbnailDefaultWidth = 270;
+  static const _thumbnailDefaultHeight = 480;
 
   static const _originalUrlKey = 'original_url';
   static const _thumbnailUrlKey = 'thumbnail_url';
@@ -27,18 +31,34 @@ class ImageDataSource implements ImageDataSourceImpl {
       String documentId, Asset asset) async {
     final uuid = Uuid().v4();
     final metadata = StorageMetadata(contentType: 'image/jpeg');
-    final originalByteData =
-        await asset.getByteData(quality: _originalImageQuality);
+
+    ByteData originalByteData;
+    ByteData thumbnailByteData;
+
+    if (asset.originalWidth >= asset.originalHeight) {
+      originalByteData = await asset.getThumbByteData(
+          _originalDefaultHeight, _originalDefaultWidth,
+          quality: _originalImageQuality);
+      thumbnailByteData = await asset.getThumbByteData(
+          _thumbnailDefaultHeight, _thumbnailDefaultWidth,
+          quality: _thumbnailImageQuality);
+    } else {
+      originalByteData = await asset.getThumbByteData(
+          _originalDefaultWidth, _originalDefaultHeight,
+          quality: _originalImageQuality);
+      thumbnailByteData = await asset.getThumbByteData(
+          _thumbnailDefaultWidth, _thumbnailDefaultHeight,
+          quality: _thumbnailImageQuality);
+    }
+
     final originalData = originalByteData.buffer.asUint8List();
     final originalReference =
         _storage.ref().child('$documentId/original/$uuid.jpg');
-    final thumbnailByteData = await asset.getThumbByteData(
-        (asset.originalWidth * _thumbnailImageRate).round(),
-        (asset.originalHeight * _thumbnailImageRate).round(),
-        quality: _thumbnailImageQuality);
+
     final thumbnailData = thumbnailByteData.buffer.asUint8List();
     final thumbnailReference =
         _storage.ref().child('$documentId/thumbnail/$uuid-thumb.jpg');
+
     final originalUploadTask =
         originalReference.putData(originalData, metadata);
     final thumbnailUploadTask =
