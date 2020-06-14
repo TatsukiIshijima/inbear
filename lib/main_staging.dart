5a1d2d168cfcb7476b815932b60dfd3bf299e516
ishijimaitsuki <tatsuki445@gmail.com>
2020-06-14 20:28:43 +0900

    iOSでStaging環境設定

---
+++ lib/main_staging.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:inbear_app/flavor.dart';
+import 'package:inbear_app/inbear_app.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  runApp(Provider<Flavor>.value(
+    value: Flavor.staging,
+    child: InbearApp(),
+  ));
+}
